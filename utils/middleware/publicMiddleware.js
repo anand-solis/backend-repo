3db6247cbd1967dcd99614932c79144994312bc7
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-10 15:20:25 +0530

    Public Middleware Added

--- utils/middleware/publicMiddleware.js
+++ utils/middleware/publicMiddleware.js
@@ -1,5 +1,13 @@
-const PublicMiddleware = async (req, res, next) => {
-    next();
-}
+const PublicMiddleware = (req, res, next) => {
+    const apiToken = process.env.PUBLIC_API_TOKEN;
+    const { token } = req.body;
+
+    if (apiToken == token) {
+        next();
+    }
+    else{
+        return res.status(403).send("You don't have permission to do this task!");
+    }
+};
 
 module.exports = PublicMiddleware;
