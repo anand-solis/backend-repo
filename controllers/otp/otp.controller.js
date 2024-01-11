3c690b0ab34abbf30b0a6458f4c9498bf69860ce
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-11 12:42:33 +0530

    Email and sms Templates update

--- controllers/otp/otp.controller.js
+++ controllers/otp/otp.controller.js
@@ -2,8 +2,9 @@ const OTP = require("@/models/account/otp.model");
 const validateParam = require("@/controllers/otp/validateParam.controller");
 const generateCode = require("@/controllers/otp/generateCode.controller");
 const sendEmailController = require("@/controllers/email/sendEmail.controller");
-const SMSController = require("@/controllers/sms/sms.controller");
-const VerificationOTP = require("@/emails/VerificationOTP.template");
+const sendSMSController = require("@/controllers/sms/sendSMS.controller");
+const EmailVerificationOTP = require("@/templates/emails/EmailVerificationOTP.template");
+const SMSVerificationOTP = require("@/templates/sms/SMSVerificationOTP.template");
 
 const OTPController = async (req, res) => {
     const { param } = req.body;
@@ -16,11 +17,11 @@ const OTPController = async (req, res) => {
             otp.save();
 
             if (paramType.type == "email") {
-                const sendEmail = await sendEmailController(param, "Verification OTP", VerificationOTP(code));
+                const sendEmail = await sendEmailController(param, "Verification OTP", EmailVerificationOTP(code));
                 return res.json({ success: sendEmail.success, error: sendEmail.error, message: sendEmail.message });
             }
             else if (paramType.type == "phone") {
-                const sendSMS = await SMSController(param, code);
+                const sendSMS = await sendSMSController(param, SMSVerificationOTP(code));
                 return res.json({ success: sendSMS.success, error: sendSMS.error, message: sendSMS.message });
             }
         } catch (error) {
--- templates/emails/EmailVerificationOTP.template.js
+++ templates/emails/EmailVerificationOTP.template.js
@@ -1,4 +1,4 @@
-const VerificationOTP = (code) => {
+const EmailVerificationOTP = (code) => {
     const template = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html dir="ltr" lang="en">
         <head>
@@ -32,4 +32,4 @@ const VerificationOTP = (code) => {
 
     return template;
 }
-module.exports = VerificationOTP
+module.exports = EmailVerificationOTP;
