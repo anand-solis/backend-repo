b8fb0d2edb55b897657a3022154af4a9f872790c
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-12 11:40:19 +0530

    Update Profile API for check verified phone or email

--- controllers/profile/profile.controller.js
+++ controllers/profile/profile.controller.js
@@ -3,19 +3,34 @@ const User = require("@/models/account/users.model");
 const ProfileController = async (req, res) => {
     const { name, email, phone, role } = req.body;
     try {
-        const user = await User.findOneAndUpdate(
-            { _id: req?.user?._id },
-            {
+        let data = {};
+        
+        if (req?.user?.email) {
+            data = {
                 name: name,
-                email: {
-                    address: email.address
-                },
                 phone: {
-                    number: phone.number
+                    number: phone
                 },
                 role: role
             }
-        );
+        }
+        if(req?.user?.phone){
+            data = {
+                name: name,
+                email: {
+                    address: email
+                },
+                role: role
+            }
+        }
+        if(req?.user?.email && req?.user?.phone){
+            data = {
+                name: name,
+                role: role
+            }
+        }
+
+        await User.findOneAndUpdate({ _id: req?.user?._id }, data);
         return res.json({ success: true, error: "" });
     } catch (error) {
         return res.json({ success: false, error: `Error: ${error}` });
