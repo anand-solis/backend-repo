31542e83d2be1d71932adb29c7675e1d9abfe3a5
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-19 15:41:07 +0530

    Permission details controller created

--- controllers/organization/permission/getPermission.controller.js
+++ controllers/organization/permission/getPermission.controller.js
@@ -1,5 +1,26 @@
 const getPermissionController = async (req, res) => {
+    const { organization } = req.body;
+    const { id } = req.params;
+    try{
+        const permission = await Permission
+        .findOne({ _id: id, organization: organization })
+        .sort({ name: 1 })
+        .select("name features")
+        .populate({
+            path: "features.feature",
+            select: ["name", "key"]
+        })
+        .populate({
+            path: "createdBy",
+            select: "name"
+        });
 
+        return res.json({ permission: permission, success: true, error: "", message: "permission details fetched successfully" });
+    } catch(error) {
+        return res.json({ permission: null, success: false, error: `Error: ${error}`, message: "" });
+    }
 }
 
 module.exports = getPermissionController;
+
+const Permission = require("@/models/organization/permissions.model");
