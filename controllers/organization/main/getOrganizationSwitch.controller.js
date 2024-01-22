faa7696cfd66370de4656f53664b4bc532760290
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-22 17:24:20 +0530

    getOrganizationSwitch changes

--- controllers/organization/main/getOrganizationSwitch.controller.js
+++ controllers/organization/main/getOrganizationSwitch.controller.js
@@ -1,10 +1,19 @@
+const Member = require("@/models/organization/members.model");
 const Organization = require("@/models/organization/organization.model");
 
 const getOrganizationSwitchController = async (req, res) => {
     try {
-        const organizations = await Organization.find({}).select("name").sort({ createdAt: -1 });
+        const members = await Member.find({ user: req?.user?._id }).select("organization");
+        if(members.length == 0){
+            return res.json({ organizations: null, success: false, error: "You are not any organization.", message: "" });
+        }
+        else{
+            const haveOrganizationIds = members.map(member => member.organization);
 
-        return res.json({ organizations: organizations, success: true, error: "", message: "Organizations records fetched successfully." });
+            const organizations = await Organization.find({ _id: { $in: haveOrganizationIds } }).select("name").sort({ createdAt: -1 });
+    
+            return res.json({ organizations: organizations, success: true, error: "", message: "Organizations records fetched successfully." });
+        }
     } catch (error) {
         return res.json({ organizations: null, success: false, error: `Error: ${error}`, message: "" });
     }
--- routes/organization/organization.route.js
+++ routes/organization/organization.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const OrganizationController = require("@/controllers/organization/main/organization.controller");
 const getOrganizationSwitchController = require("@/controllers/organization/main/getOrganizationSwitch.controller");
-const organizationMemberMiddleware = require("@/utils/middleware/organization/organizationMemberMiddleware");
 
 const Middleware = require("@/utils/middleware/middleware");
 
@@ -10,7 +9,6 @@ const router = express.Router();
 router.get(
     "/organization/switch",
     Middleware,
-    organizationMemberMiddleware,
     getOrganizationSwitchController
 );
 
