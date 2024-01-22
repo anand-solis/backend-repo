a49a8cc3331889ac326c74cc133ca6ee8ecb2944
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-22 15:58:01 +0530

    Documentation updates

--- swagger.js
+++ swagger.js
@@ -5,22 +5,22 @@ const docs = {
         title: "Solis Construction Backend API Documentation",
         description: "Solis Construction Backend API Documentation"
     },
-    host: "localhost:5000"
+    host: "http://localhost:5000"
 };
 
 const outputFile = "./docs/swagger.json";
 
 const routes = [
-    "routes/account/login.route.js",
     "routes/account/otp.route.js",
+    "routes/account/login.route.js",
     "routes/account/role.route.js",
     "routes/account/profile.route.js",
     "routes/app/email.route.js",
     "routes/app/sms.route.js",
     "routes/organization/organization.route.js",
     "routes/organization/permission.route",
-    "routes/organization/site.route",
-    "routes/organization/invite.route"
+    "routes/organization/invite.route",
+    "routes/organization/site.route"
 ];
 
 swaggerAutogen(outputFile, routes, docs);
