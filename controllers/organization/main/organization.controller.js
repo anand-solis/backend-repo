6bf09e7fd90e1c4d614b2f8f886ca97a57e99c1d
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-24 13:55:44 +0530

    Successful Organization email send added

--- controllers/organization/main/organization.controller.js
+++ controllers/organization/main/organization.controller.js
@@ -2,17 +2,22 @@ const mongoose = require("mongoose");
 const createOrganization = require("@/controllers/organization/main/createOrganization.controller");
 const createDefaultPermissions = require("@/controllers/organization/main/createDefaultPermissions.controller");
 const createSubscriptionPlan = require("@/controllers/organization/main/createSubscriptionPlan.controller");
+const sendEmailController = require("@/controllers/app/email/sendEmail.controller");
+const SuccessCreateOrganization = require("@/templates/emails/SuccessCreateOrganization.template");
 
 const OrganizationController = async (req, res) => {
     const session = await mongoose.startSession();
     session.startTransaction();
 
+    const { name, email, phone } = req.body;
+    
     try {
         const organization = await createOrganization(req, session);
 
         if (organization.success) {
             await createSubscriptionPlan(organization.id, session);
             await createDefaultPermissions(organization.id, req?.user?._id, session);
+            await sendEmailController(email, "Organization Successfully Created", SuccessCreateOrganization(name, email, phone));
 
             await session.commitTransaction();
             session.endSession();
