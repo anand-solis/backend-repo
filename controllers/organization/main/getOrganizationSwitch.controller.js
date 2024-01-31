fe1a8481b7c645985ccbe2ef0f4a2fd48b10f52c
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-01-31 11:59:01 +0530

    Give organization inviteAccept field added

--- controllers/organization/main/getOrganizationSwitch.controller.js
+++ controllers/organization/main/getOrganizationSwitch.controller.js
@@ -3,7 +3,7 @@ const Organization = require("@/models/organization/organization.model");
 
 const getOrganizationSwitchController = async (req, res) => {
     try {
-        const members = await Member.find({ user: req?.user?._id }).select("organization");
+        const members = await Member.find({ user: req?.user?._id }).select("organization inviteAccepted");
         if(members.length == 0){
             return res.status(204).json({ organizations: null, success: false, error: "You are not any organization.", message: "" });
         }
@@ -11,8 +11,20 @@ const getOrganizationSwitchController = async (req, res) => {
             const haveOrganizationIds = members.map(member => member.organization);
 
             const organizations = await Organization.find({ _id: { $in: haveOrganizationIds } }).select("name").sort({ createdAt: -1 });
-    
-            return res.status(200).json({ organizations: organizations, success: true, error: "", message: "Organizations fetched successfully." });
+
+            let organizationsWithInviteStatus = [];
+
+            organizations.map((organization) => {
+                const index = members ? members.findIndex(item => item.organization.toString() == organization._id.toString()) : -1;
+
+                organizationsWithInviteStatus.push({
+                    _id: organization._id,
+                    name: organization.name,
+                    inviteAccepted: members[index].inviteAccepted
+                });
+            })
+
+            return res.status(200).json({ organizations: organizationsWithInviteStatus, success: true, error: "", message: "Organizations fetched successfully." });
         }
     } catch (error) {
         return res.status(500).json({ organizations: null, success: false, error: `Error: ${error}`, message: "" });
