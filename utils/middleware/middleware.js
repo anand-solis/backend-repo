4dc3acb0aaaf7d4350414a17e46929345be47fc4
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-01 18:15:50 +0530

    JWT error handling

--- utils/middleware/middleware.js
+++ utils/middleware/middleware.js
@@ -30,21 +30,25 @@ const Middleware = async (req, res, next) => {
                             req.user = cachedValue;
                             next();
                         } else {
-                            const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+                            try {
+                                const decoded = await jwt.verify(token, process.env.JWT_SECRET);
             
-                            const _id = new mongoose.Types.ObjectId(decoded?._doc?._id);
-            
-                            const user = await User.findOne({ _id: _id });
-                            if (!user) {
-                                return res.status(401).json({ success: false, error: "User account not found." });
+                                const _id = new mongoose.Types.ObjectId(decoded?._doc?._id);
+                
+                                const user = await User.findOne({ _id: _id });
+                                if (!user) {
+                                    return res.status(401).json({ success: false, error: "User account not found." });
+                                }
+                                if (user.blocked) {
+                                    return res.status(401).json({ success: false, error: "User account blocked." });
+                                }
+                                user.token = token;
+                                req.user = user;
+                                myCache.set("userData", req.user, 7200); // Cache for 2 hour (7200 seconds)
+                                next();
+                            } catch (error) {
+                                return res.status(401).json({ success: false, error: "Unauthorized user." });
                             }
-                            if (user.blocked) {
-                                return res.status(401).json({ success: false, error: "User account blocked." });
-                            }
-                            user.token = token;
-                            req.user = user;
-                            myCache.set("userData", req.user, 7200); // Cache for 2 hour (7200 seconds)
-                            next();
                         }
                     }
                 });
