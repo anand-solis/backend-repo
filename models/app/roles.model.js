a662e008b28e3334072c21efaa7a72f436ec6e1d
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-05 10:04:19 +0530

    role name must be unique

--- models/app/roles.model.js
+++ models/app/roles.model.js
@@ -3,9 +3,19 @@ const mongoose = require("mongoose");
 const RoleSchema = mongoose.Schema({
     name: {
         type: String,
-        unique: [true, "Role name must be unique."],
         required: [true, "Role name is required."]
     }
 }, { timestamps: true });
+
+RoleSchema.pre("save", async function (next) {
+    const role = this;
+    const existingRole = await mongoose.model("Role").findOne({ name: role.name });
+
+    if (existingRole) {
+        next("Role name must be unique.");
+    } else {
+        next();
+    }
+});
 
 module.exports = mongoose.model("Role", RoleSchema);
