9dde60c971e228956fa8d04818db2178962f2fa5
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-06 10:03:25 +0530

    Cache Removed for user login check

--- utils/middleware/middleware.js
+++ utils/middleware/middleware.js
@@ -1,10 +1,8 @@
 const User = require("@/models/account/users.model");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
-require("dotenv").config();
 const connectSqliteDB = require("@/utils/connections/database/connectSqliteDB");
+require("dotenv").config();
 
 const Middleware = async (req, res, next) => {
     const authHeader = req.headers["authorization"];
@@ -24,32 +22,21 @@ const Middleware = async (req, res, next) => {
                     else if (row?.token) {
                         return res.status(400).json({ success: false, expired: true, error: "This token is expired." });
                     }
-                    else{
-                        const cachedValue = myCache.get("userData");
-                        if (myCache.has("userData") && cachedValue.token == token) {
-                            req.user = cachedValue;
-                            next();
-                        } else {
-                            try {
-                                const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-            
-                                const _id = new mongoose.Types.ObjectId(decoded?._doc?._id);
-                
-                                const user = await User.findOne({ _id: _id });
-                                if (!user) {
-                                    return res.status(401).json({ success: false, error: "User account not found." });
-                                }
-                                if (user.blocked) {
-                                    return res.status(401).json({ success: false, error: "User account blocked." });
-                                }
-                                user.token = token;
-                                req.user = user;
-                                myCache.set("userData", req.user, 7200); // Cache for 2 hour (7200 seconds)
-                                next();
-                            } catch (error) {
-                                return res.status(401).json({ success: false, error: "Unauthorized user." });
-                            }
+                    else {
+                        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+                        const _id = new mongoose.Types.ObjectId(decoded?._doc?._id);
+
+                        const user = await User.findOne({ _id: _id });
+                        if (!user) {
+                            return res.status(401).json({ success: false, error: "User account not found." });
                         }
+                        if (user.blocked) {
+                            return res.status(401).json({ success: false, error: "User account blocked." });
+                        }
+                        user.token = token;
+                        req.user = user;
+                        next();
                     }
                 });
             });
