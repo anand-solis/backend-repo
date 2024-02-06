f627b837b903575af24538d6361f92db9b33b636
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-06 22:19:14 +0530

    Add new subscription API created

--- controllers/app/subscription/addSubscription.controller.js
+++ controllers/app/subscription/addSubscription.controller.js
@@ -1,5 +1,26 @@
-const AddSubscriptionController = async (req, res) => {
+const Subscription = require("@/models/app/subscription.model");
 
+const AddSubscriptionController = async (req, res) => {
+    const { title, sub_title, description, price, sites_count, users_count, admin_settings, project_management } = req.body;
+
+    try {
+        const newSubscription = new Subscription({
+            title: title,
+            sub_title: sub_title,
+            description: description,
+            price: price,
+            sites_count: sites_count,
+            users_count: users_count,
+            admin_settings: admin_settings,
+            project_management: project_management
+        })
+
+        await newSubscription.save();
+
+        return res.status(201).json({ success: true, error: "", message: "New Subscription created successfully." });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: `Error: ${error}`, message: "" });
+    }
 }
 
 module.exports = AddSubscriptionController;
