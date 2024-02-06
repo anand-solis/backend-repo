85cdfb15144a83b4f3d6d3e6fcfae8b22a265ec8
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-06 15:41:52 +0530

    Get All Subscriptions API added

--- routes/app/subscription.route.js
+++ routes/app/subscription.route.js
@@ -3,6 +3,7 @@ const Middleware = require("@/utils/middleware/middleware");
 const superAdminMiddleware = require("@/utils/middleware/superAdminMiddleware");
 
 const GetSubscriptionsCountController = require("../../controllers/app/subscription/getSubscriptionsCount.controller");
+const GetAllSubscriptionsController = require("../../controllers/app/subscription/getAllSubscriptions.controller");
 
 const router = express.Router();
 
@@ -11,6 +12,13 @@ router.get(
     Middleware,
     superAdminMiddleware,
     GetSubscriptionsCountController
+);
+
+router.get(
+    "/subscriptions",
+    Middleware,
+    superAdminMiddleware,
+    GetAllSubscriptionsController
 );
 
 module.exports = router;
