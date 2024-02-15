ad50b3b75cde291676517f384312af6615fd5543
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-15 11:56:17 +0530

    Get Plan by User API added

--- routes/organization/plan.route.js
+++ routes/organization/plan.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const Middleware = require("@/utils/middleware/middleware");
 const organizationMiddleware = require("@/utils/middleware/organization/organizationMiddleware");
+const organizationMemberMiddleware = require("@/utils/middleware/organization/organizationMemberMiddleware");
+
 const getPlanController = require("../../controllers/organization/plan/getPlan.controller");
+const getPlanByUserController = require("../../controllers/organization/plan/getPlanByUser.controller");
 
 const router = express.Router();
 
@@ -13,6 +16,13 @@ router.get(
     Middleware,
     (req, res, next) => organizationMiddleware(req, res, next, key, "read", plan),
     getPlanController
+);
+
+router.get(
+    "/plan/user",
+    Middleware,
+    (req, res, next) => organizationMemberMiddleware(req, res, next),
+    getPlanByUserController
 );
 
 module.exports = router;
