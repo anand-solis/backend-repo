429261cb51782be2c83bacf82e1f39fe92d43e0a
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-16 16:12:19 +0530

    Try Catch added in Site and Task middleware

--- utils/middleware/organization/taskMiddleware.js
+++ utils/middleware/organization/taskMiddleware.js
@@ -4,32 +4,36 @@ const TaskMember = require("@/models/organization/site/task/taskMember.model");
 const taskMiddleware = async (req, res, next) => {
     const { organization, site, task } = req.query;
 
-    const taskDetails = await Task.findOne({ _id: task, site: site, organization: organization }).select("_id");
+    try {
+        const taskDetails = await Task.findOne({ _id: task, site: site, organization: organization }).select("_id");
 
-    if (taskDetails?._id) {
-        const MemberDetails = await TaskMember
-            .findOne({ task: taskDetails?._id })
-            .select("_id member")
-            .populate({
-                path: "member",
-                select: "member",
-                populate: {
+        if (taskDetails?._id) {
+            const MemberDetails = await TaskMember
+                .findOne({ task: taskDetails?._id })
+                .select("_id member")
+                .populate({
                     path: "member",
-                    match: {
-                        user: req.user._id
+                    select: "member",
+                    populate: {
+                        path: "member",
+                        match: {
+                            user: req.user._id
+                        }
                     }
-                }
-            });
-
-        if (MemberDetails?._id && MemberDetails?.member?.member) {
-            next();
+                });
+    
+            if (MemberDetails?._id && MemberDetails?.member?.member) {
+                next();
+            }
+            else {
+                return res.status(409).json({ success: false, error: "You are not a member of this task." });
+            }
         }
         else {
-            return res.status(409).json({ success: false, error: "You are not a member of this task." });
+            return res.status(409).json({ success: false, error: "This task not exist in your organization." });
         }
-    }
-    else {
-        return res.status(409).json({ success: false, error: "This task not exist in your organization." });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: `Error: ${error}` });
     }
 }
 
