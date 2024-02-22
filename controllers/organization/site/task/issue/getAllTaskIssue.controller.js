96b80fa6df1b918f2ba38457968ff830965ecd25
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-22 11:53:05 +0530

    All Task APIs including GET, UPDATE, REMOVE & ADD added

--- controllers/organization/site/task/issue/getAllTaskIssue.controller.js
+++ controllers/organization/site/task/issue/getAllTaskIssue.controller.js
@@ -1,5 +1,20 @@
-const GetAllTaskIssueController = async (req, res) => {
+const TaskIssue = require("@/models/organization/site/task/taskIssue.model");
 
+const GetAllTaskIssueController = async (req, res) => {
+    const { organization, site, floor, task } = req.query;
+
+    try {
+        const taskIssues = await TaskIssue.find({
+            organization: organization,
+            site: site,
+            floor: floor,
+            task: task
+        });
+
+        return res.status(200).json({ taskIssues: taskIssues, success: true, error: "", message: "Task issue fetched successfully." });
+    } catch (error) {
+        return res.status(500).json({ taskIssues: null, success: false, error: `Error: ${error}`, message: "" });
+    }
 }
 
 module.exports = GetAllTaskIssueController;
