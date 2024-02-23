9245ad991ce23008b4b1c1778515ba167f80a7ca
Gourav Nagpal <gouravnagpal@solistechnology.in>
2024-02-23 16:29:17 +0530

    Purchase Order Settings model added

---
+++ models/organization/site/purchaseOrder/purchaseOrderSetting.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const PurchaseOrderSettingSchema = mongoose.Schema({
+    organization: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Organization",
+        required: true
+    },
+    site: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Site",
+        required: true
+    },
+    prefix: {
+        type: String,
+        required: true,
+        default: "PO-"
+    },
+    tnc: {
+        type: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("PurchaseOrderSetting", PurchaseOrderSettingSchema);
