c16b12f21a207838585096a331feeb8f5964f37d
ayushsolistech <sharma.ayush@solistechnology.in>
2024-04-25 14:19:43 +0530

    API of Expnses Bills Deatils

---
+++ controllers/organization/bills/getExpense.controller.js
@@ -0,0 +1,23 @@
+const ExpenseBill = require("@/models/organization/main/bills/addExpense.Bill.model");
+
+const getExpenseBill = async (req, res) => {
+  const { id, organization } = req.query;
+  try {
+    const data = await ExpenseBill.find({
+      organization: organization,
+    });
+    console.log(data);
+    return res.status(200).json({
+      success: true,
+      data: data,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: err,
+      message: "Failed to get Site Expenses.",
+    });
+  }
+};
+
+module.exports = getExpenseBill;
