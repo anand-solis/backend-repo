251bb87ecc4a565564145421e1b6467f9969df73
anand-solis <anand@solistechnology.in>
2024-06-04 18:36:59 +0530

    make changes in read image utils

--- utils/connections/storage/getStorageFile.js
+++ utils/connections/storage/getStorageFile.js
@@ -1,14 +1,22 @@
-const storage = require("@/utils/connections/storage/connectStorage");
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_ACCESS_SECRET,
+    endpoint: new AWS.Endpoint('https://3e7cafe54ca91ebf65e89a8dc2ca5aa1.r2.cloudflarestorage.com'),
+    region: 'auto',
+    s3ForcePathStyle: true, // needed for custom endpoint
+    signatureVersion: 'v4' // Ensure the signature version is v4
+});
 
 const getStorageFile = async (key) => {
-    const params = {
-        Bucket: process.env.S3_BUCKET,
-        Key: key,
-        Expires: 20
-    };
-
     try {
-        const url = storage.getSignedUrl("getObject", params);
+        const params = {
+            Bucket: process.env.S3_BUCKET,
+            Key: key,
+            Expires: 200000
+        };
+        const url = s3.getSignedUrl("getObject", params);
+        console.log("url..................", url)
 
         return { file: url, success: true, error: "", message: "File fetched successfully." };
     } catch (error) {
