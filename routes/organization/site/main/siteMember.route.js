3f3c654a86c15d1e11612454c5f38f0993efe6a8
anand-solis <anand@solistechnology.in>
2024-06-05 09:15:24 +0530

    new api for assign member and site member

--- routes/organization/site/main/siteMember.route.js
+++ routes/organization/site/main/siteMember.route.js
@@ -10,6 +10,8 @@ const inviteSiteMemberController = require("../../../../controllers/organization
 const inviteAcceptSiteMemberController = require("../../../../controllers/organization/site/member/invite/inviteAcceptSiteMember.controller");
 const inviteRejectSiteMemberController = require("../../../../controllers/organization/site/member/invite/inviteRejectSiteMember.controller");
 const resendInviteSiteMemberController = require("../../../../controllers/organization/site/member/invite/resendInviteSiteMember.controller");
+const getSiteMembers = require("../../../../controllers/organization/site/main/getSitemember.controller");
+const inviteMemberController = require("../../../../controllers/organization/member/getInvitationAcceptedMembers.controllers")
 
 
 const router = express.Router();
@@ -63,5 +65,19 @@ router.post(
     (req, res, next) => siteMiddleware(req, res, next, true),
     inviteAcceptSiteMemberController
 );
+router.get(
+    "/site/getMembers",
+    Middleware,
+    (req, res, next) => organizationMemberMiddleware(req, res, next, true),
+    (req, res, next) => siteMiddleware(req, res, next, true),
+    getSiteMembers
+);
+router.get(
+    "/site/getInvitationAcceptMembers",
+    Middleware,
+    (req, res, next) => organizationMemberMiddleware(req, res, next, true),
+    inviteMemberController
+);
+
 
 module.exports = router;
