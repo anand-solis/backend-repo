4e3a2392b2839b73271b4fec9ce25a32f4dbac02
anand-solis <anand@solistechnology.in>
2024-06-05 11:41:09 +0530

    organiztion can add more than 10 sites

--- controllers/organization/site/main/createSite.controller.js
+++ controllers/organization/site/main/createSite.controller.js
@@ -35,53 +35,90 @@ const CreateSiteController = async (req, res) => {
                 path: "subscription",
                 select: "sites_count"
             });
+
+    //
         const createdSites = await Site.find({ organization: organization }).select("_id");
         const createdSitesCount = createdSites.length;
+        const newSite = new Site({
+            name: SiteName,
+            startDate: siteStartDate,
+            endDate: siteEndDate,
+            organization: organization,
+            createdBy: req?.user?._id,
+            profile: response.file
+        });
 
-        if (plan?.subscription?.sites_count) {
-            if (createdSitesCount < plan?.subscription?.sites_count) {
-                const newSite = new Site({
-                    name: SiteName,
-                    startDate: siteStartDate,
-                    endDate: siteEndDate,
+        const newSiteResponse = await newSite.save();
+        if (newSiteResponse?._id) {
+            const OrganizationMember = await Member.findOne({ organization: organization, user: req.user._id }).select("_id");
+
+            if (OrganizationMember?._id) {
+                const NewSiteMember = new SiteMember({
+                    site: newSiteResponse?._id,
                     organization: organization,
-                    createdBy: req?.user?._id,
-                    profile: response.file
-                });
+                    member: OrganizationMember._id,
+                    isCreator: true,
+                    inviteAccepted: true
+                })
 
-                const newSiteResponse = await newSite.save();
-                console.log("newSiteResponse...........", newSiteResponse)
-                if (newSiteResponse?._id) {
-                    const OrganizationMember = await Member.findOne({ organization: organization, user: req.user._id }).select("_id");
+                await NewSiteMember.save();
 
-                    if (OrganizationMember?._id) {
-                        const NewSiteMember = new SiteMember({
-                            site: newSiteResponse?._id,
-                            organization: organization,
-                            member: OrganizationMember._id,
-                            isCreator: true,
-                            inviteAccepted: true
-                        })
-
-                        await NewSiteMember.save();
-
-                        return res.status(201).json({ site: newSiteResponse?._id, success: true, error: "", message: "Site successfully created." });
-                    }
-                    else {
-                        return res.status(401).json({ site: null, success: false, error: "You are not a member of this organization.", message: "" });
-                    }
-                }
-                else {
-                    return res.status(400).json({ site: null, success: false, error: "Site not created.", message: "" });
-                }
+                return res.status(201).json({ site: newSiteResponse?._id, success: true, error: "", message: "Site successfully created." });
             }
             else {
-                return res.status(401).json({ site: null, success: false, error: "Error: You exceed limit to create site in your organization, Upgrade your subscription plan.", message: "" });
+                return res.status(401).json({ site: null, success: false, error: "You are not a member of this organization.", message: "" });
             }
         }
         else {
-            return res.status(401).json({ site: null, success: false, error: "Error: You don't have this features in your subscription plan.", message: "" });
+            return res.status(400).json({ site: null, success: false, error: "Site not created.", message: "" });
         }
+
+        //
+
+        // if (plan?.subscription?.sites_count) {
+        //     if (createdSitesCount < plan?.subscription?.sites_count) {
+        //         const newSite = new Site({
+        //             name: SiteName,
+        //             startDate: siteStartDate,
+        //             endDate: siteEndDate,
+        //             organization: organization,
+        //             createdBy: req?.user?._id,
+        //             profile: response.file
+        //         });
+
+        //         const newSiteResponse = await newSite.save();
+        //         console.log("newSiteResponse...........", newSiteResponse)
+        //         if (newSiteResponse?._id) {
+        //             const OrganizationMember = await Member.findOne({ organization: organization, user: req.user._id }).select("_id");
+
+        //             if (OrganizationMember?._id) {
+        //                 const NewSiteMember = new SiteMember({
+        //                     site: newSiteResponse?._id,
+        //                     organization: organization,
+        //                     member: OrganizationMember._id,
+        //                     isCreator: true,
+        //                     inviteAccepted: true
+        //                 })
+
+        //                 await NewSiteMember.save();
+
+        //                 return res.status(201).json({ site: newSiteResponse?._id, success: true, error: "", message: "Site successfully created." });
+        //             }
+        //             else {
+        //                 return res.status(401).json({ site: null, success: false, error: "You are not a member of this organization.", message: "" });
+        //             }
+        //         }
+        //         else {
+        //             return res.status(400).json({ site: null, success: false, error: "Site not created.", message: "" });
+        //         }
+        //     }
+        //     else {
+        //         return res.status(401).json({ site: null, success: false, error: "Error: You exceed limit to create site in your organization, Upgrade your subscription plan.", message: "" });
+        //     }
+        // }
+        // else {
+        //     return res.status(401).json({ site: null, success: false, error: "Error: You don't have this features in your subscription plan.", message: "" });
+        // }
 
     } catch (error) {
         console.log(error)
