7a134a63f4667174a0f8990b6870b74d2fd594ec
anand-solis <anand@solistechnology.in>
2024-06-07 10:47:32 +0530

    add pagination and search in get materials api

--- controllers/organization/material/getMaterial.controller.js
+++ controllers/organization/material/getMaterial.controller.js
@@ -1,17 +1,69 @@
 const material = require("@/models/organization/main/material/material.modal");
+const pagination= require("../../../utils/pagination");
+const { default: mongoose } = require("mongoose");
 
 const getAllmaterialdata = async (req, res) => {
-  const { organization } = req.query;
 
   try {
-    const materials = await material.find({ organization: organization });
-
+    const { organization, bom,page,limit } = req.query;
+    let materialdata
+    let {skip,perPage} = await pagination(page,limit)
+    let matchObj = {
+      organization:new mongoose.Types.ObjectId(organization)
+    }
+    if (bom) {
+      matchObj["uom"] = {
+        $regex:bom,
+        $options:"i"
+      }
+    }
+    
+    let facet = {
+      total:[
+        {
+          $match:matchObj
+        },
+          {
+              '$count': 'total'
+          }
+      ],
+      data:[
+          {
+              $match:matchObj
+          },
+          {
+              $skip:skip
+          },
+          {
+              $limit:perPage
+          }
+      ]
+  }
+  let aggregate = []
+  aggregate.push({
+      $facet:facet
+  })
+  materialdata= await material.aggregate(aggregate)
+  materialdata = materialdata[0]
+  let total = materialdata?.total.length?materialdata?.total[0]?.total:0
+  let data = materialdata?.data.length?materialdata?.data:[]
+  if(data.length){
     return res.status(200).json({
       success: true,
-      data: materials,
+      data: data,
+      total:total,
       message: "Materials found successfully.",
     });
+  }
+  return res.status(404).json({
+    success: false,
+    data: [],
+    total:total,
+    message: "Materials not found ",
+  });
+    
   } catch (err) {
+    console.log(err)
     return res.status(500).json({
       success: false,
       message: "Failed to get Material.",
