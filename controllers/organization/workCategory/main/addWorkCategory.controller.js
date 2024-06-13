6c52e9e27bd4eb3776fc825f09f746b9c201b8e4
anand-solis <anand@solistechnology.in>
2024-06-13 10:01:37 +0530

    make work category uniuqe for the organization

--- controllers/organization/workCategory/main/addWorkCategory.controller.js
+++ controllers/organization/workCategory/main/addWorkCategory.controller.js
@@ -6,6 +6,13 @@ const AddWorkCategoryController = async (req, res) => {
     const { name } = req.body;
 
     try {
+        let workCategory = await WorkCategory.find({
+            name:name,
+            organization:organization
+        })
+        if(workCategory.length){
+            return res.status(409).json({ success: false, error: "", message: "Work category already exists for this organization." });
+        }
         await WorkCategory.create({
             organization: organization,
             name: name,
